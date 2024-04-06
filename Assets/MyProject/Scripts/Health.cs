181f83e226be02e13c92a76d343e3407cd43a1b9
a.bogunov <bogunov_aa@edu.bstu.ru>
2024-04-06 19:46:09 +0300

    web2

---
+++ Assets/MyProject/Scripts/Health.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth;
+    [SerializeField] private Animator _animator;
+
+    private float _currentHealth;
+    private bool _isDead;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if (_isDead) return;
+
+        _currentHealth -= damage;
+
+        if (_currentHealth < 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        _isDead = true;
+        _animator.SetTrigger("Dead");
+    }
+}
